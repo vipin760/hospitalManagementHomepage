9d542eb584a2205d9939f4eb08263c2928e1c264
vipin_760 <vipinm500@gmail.com>
2023-12-30 14:39:44 +0530

    icons section starting

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ let menu = document.querySelector('#menu-btn')
 let navbar = document.querySelector('.navbar')
 
 menu.onclick = () => {
-    menu.classList.toggle('fa-times')
+    menu.classList.toggle('fa-times') 
     navbar.classList.toggle('active')
 }
 window.onscroll=()=>{
